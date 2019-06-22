81e4bd4c126db4096ed0c8baa316897ae54193cb
Hugo Tavares <hugo.m.tavares@gmail.com>
2019-06-22 10:40:20 +0100

    add setup script

---
+++ setup.sh
@@ -0,0 +1,29 @@
+
+#### R package dependencies ####
+
+# from CRAN
+Rscript -e 'install.packages(c("tidyverse", "corrplot", "cowplot", "ggfortify"))'
+
+# from BioConductor
+Rscript -e 'install.packages("BiocManager"); BiocManager::install("ComplexHeatmap")'
+
+
+#### Day 1&2 data ####
+
+# Create data directories
+mkdir -p day1_intro_r/data
+
+# download data
+wget -O day1_intro_r/data/portal_data_joined.csv https://ndownloader.figshare.com/files/2292169
+wget -O day1_intro_r/data/survey_data_spreadsheet_messy.xls https://ndownloader.figshare.com/files/2252083
+
+
+#### Day 4 data ####
+
+# Create directory
+mkdir -p day4_rnaseq_analysis/data
+
+for FILE in counts_raw.csv counts_transformed.csv sample_info.csv test_result.csv
+do
+  wget -O day4_rnaseq_analysis/data/${FILE} "https://github.com/tavareshugo/data-carpentry-rnaseq/blob/master/data/${FILE}?raw=true"
+done
